c9913d1e5a5c160f78c4d8fb3c29a3991df438bb
AdamCousins <Adam@KAPSYD-LTADAM.au.kaplan.local>
2016-07-01 19:55:14 +1000

    fix if condition

--- manifests/instance/app.pp
+++ manifests/instance/app.pp
@@ -64,7 +64,7 @@ define wordpress::instance::app (
   }
   
   ## tar.gz. file name lang-aware
-  if $wp_lang {
+  if $wp_lang != '' {
     $install_file_name = "wordpress-${version}-${wp_lang}.tar.gz"
   } elsif $version == 'latest' {
     $install_file_name = "${version}.tar.gz"
